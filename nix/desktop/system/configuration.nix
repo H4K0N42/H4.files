0bd689b98f97b77170616d2116d6bf9dcd957eaa
Hagen <78751562+H4K0N42@users.noreply.github.com>
2025-10-13 23:56:16 +0200

    .

--- nix/desktop/system/configuration.nix
+++ nix/desktop/system/configuration.nix
@@ -6,8 +6,14 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./0-SECRETS.nix
-    ./0-loader.nix
+    ./1-config.nix
+    ./2-boot.nix
+    ./3-nvidia.nix
+    ./4-devices.nix
+    ./5-network.nix
+    ./6-locale.nix
+    ./7-packages.nix
+    ./8-ai.nix
   ];
 
   # This value determines the NixOS release from which the default
