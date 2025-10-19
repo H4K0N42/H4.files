3fb2b0c70e8ee087c39528f5727180c431189e7d
Hagen <78751562+H4K0N42@users.noreply.github.com>
2025-10-19 13:11:27 +0200

    improve update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-if zenity --question --text="Update system?"; then
-    ghostty --title=fexec -e "sudo nix flake update --flake /etc/nixos/;echo;nix flake update --flake ~/.config/home-manager/;echo;sudo nixos-rebuild switch --upgrade;echo;home-manager switch;echo;nix profile upgrade --all;echo;read"
-    # if zenity --question --text="Optimize NixOS?"; then
-    # ghostty --title=fexec -e "bash ~/scripts/nixos-opt.sh;echo;read"
-    # fi
+
+if [[ "$TERM" != "xterm-ghostty" ]]; then
+    exec ghostty --title=fexec -e "bash ~/scripts/update.sh"
 fi
 
-
-# if zenity --question --text="Update system?"; then
-#     ghostty --title=fexec -e "sudo nix-channel --update;echo;nix-channel --update;echo;nix flake update --flake ~/.config/home-manager/;echo;sudo nixos-rebuild switch --upgrade;echo;home-manager switch;echo;nix profile upgrade --all;echo;read"
-#     if zenity --question --text="Optimize NixOS?"; then
-#     ghostty --title=fexec -e "bash ~/scripts/nixos-opt.sh;echo;read"
-#     fi
-# fi
+if ! sudo nixos-rebuild switch --upgrade; then
+    git -C /home/hagen/H4.files/nix/desktop/system/ restore flake.lock
+    sudo nixos-rebuild switch
+fi
+echo
+nix flake update --flake ~/.config/home-manager/
+home-manager switch
+echo
+nix profile upgrade --all
+echo
+echo Done.
+read
